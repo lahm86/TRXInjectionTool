f76b61cda8549fbbb379ea56ab8c43415384f216
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:27:10 +0100

    Create Mines pushblock builder

---
+++ src/Types/TR1/Misc/TR1MinesPushblockBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1MinesPushblockBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level mines = _control1.Read($@"Resources\{TR1LevelNames.MINES}");
+        TRDictionary<TR1Type, TRModel> pushblocks = new();
+        foreach (TR1Type blockType in TR1TypeUtilities.GetPushblockTypes())
+        {
+            pushblocks[blockType] = mines.Models[blockType];
+        }
+
+        ResetLevel(mines);
+        
+        TRModel basePushblock = pushblocks[TR1Type.PushBlock1];
+        pushblocks.Remove(TR1Type.PushBlock1);
+        mines.Models = pushblocks;
+
+        foreach (TRModel pushblock in pushblocks.Values)
+        {
+            pushblock.Animations[2].Commands = basePushblock.Animations[2].Commands;
+        }
+
+        InjectionData data = InjectionData.Create(mines, InjectionType.General, "mines_pushblocks", true);
+        return new() { data };
+    }
+}
