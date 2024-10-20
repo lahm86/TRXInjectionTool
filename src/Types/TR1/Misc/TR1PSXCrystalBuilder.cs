b115c664bc0dca8a360614173f32e97724f84815
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:26:42 +0100

    Create PSX crystal builder

---
+++ src/Types/TR1/Misc/TR1PSXCrystalBuilder.cs
@@ -0,0 +1,29 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1PSXCrystalBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level caves = _control1.Read($@"Resources\{TR1LevelNames.CAVES}");
+        TRModel crystal = caves.Models[TR1Type.SavegameCrystal_P];
+        ResetLevel(caves);
+
+        caves.Models[TR1Type.SavegameCrystal_P] = crystal;
+        caves.Palette[1] = new()
+        {
+            Red = 64,
+            Green = 64,
+            Blue = 252,
+        };
+
+        crystal.Meshes[0].ColouredFaces.ToList()
+            .ForEach(f => f.Texture = 1);
+
+        InjectionData data = InjectionData.Create(caves, InjectionType.PSCrystal, "purple_crystal");
+        return new() { data };
+    }
+}
