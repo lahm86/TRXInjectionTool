6e49326f5a20a05ec4dc7ae95afe335b657e9f47
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:27:38 +0100

    Create Colosseum door builder

---
+++ src/Types/TR1/Misc/TR1ColosseumDoorBuilder.cs
@@ -0,0 +1,22 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1ColosseumDoorBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level colosseum = _control1.Read($@"Resources\{TR1LevelNames.COLOSSEUM}");
+        TRModel door = colosseum.Models[TR1Type.Door2];
+
+        ResetLevel(colosseum);
+
+        colosseum.Models[TR1Type.Door2] = door;
+        door.Animations[1].Frames[0].Rotations[0].Y = door.Animations[1].Frames[1].Rotations[0].Y;
+
+        InjectionData data = InjectionData.Create(colosseum, InjectionType.General, "colosseum_door", true);
+        return new() { data };
+    }
+}
