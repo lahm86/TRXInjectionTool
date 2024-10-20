a28dd23ed820f67a540535c244de276e9f33eee8
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:28:15 +0100

    Create scion collision builder

---
+++ src/Types/TR1/Misc/TR1ScionBuilder.cs
@@ -0,0 +1,21 @@
+﻿using TRLevelControl;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1ScionBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        InjectionData data = InjectionData.Create(InjectionType.General, "scion_collision");
+        data.MeshEdits.Add(new()
+        {
+            ModelID = (uint)TR1Type.ScionPiece3_S_P,
+            MeshIndex = 0,
+            CollRadius = TRConsts.Step1 - 63,
+        });
+
+        return new() { data };
+    }
+}
