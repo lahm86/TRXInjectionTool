7c13eca819071bac7afb57969b49cefe121efddb
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:30:49 +0100

    Create PDA builder

---
+++ src/Types/TR1/Misc/TR1PDABuilder.cs
@@ -0,0 +1,17 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1PDABuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level caves = _control1.Read($@"Resources\{TR1LevelNames.CAVES}");
+        CreateModelLevel(caves, TR1Type.Map_M_U);
+
+        InjectionData data = InjectionData.Create(caves, InjectionType.General, "pda_model");
+        return new() { data };
+    }
+}
