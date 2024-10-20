b6f5069839fc42665b81ce05de09aa74aaf80396
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-20 19:22:50 +0100

    Create Uzi SFX builder

---
+++ src/Types/TR1/Misc/TR1UziSFXBuilder.cs
@@ -0,0 +1,23 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1UziSFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level caves = _control1.Read(@"Resources\LEVEL1.PHD");
+        TR1SoundEffect fx = caves.SoundEffects[TR1SFX.LaraUziFire];
+        fx.Samples = new()
+        {
+            File.ReadAllBytes(@"Resources\TR1\Uzis\0.wav"),
+        };
+        ResetLevel(caves);
+
+        caves.SoundEffects[TR1SFX.LaraUziFire] = fx;
+
+        InjectionData data = InjectionData.Create(caves, InjectionType.PSUziSFX, "uzi_sfx");
+        return new() { data };
+    }
+}
