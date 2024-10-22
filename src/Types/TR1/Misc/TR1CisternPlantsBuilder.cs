676fc8c1287c3f55f5f8d5695a96a05eb6866551
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-10-22 11:38:48 +0100

    Create animated plant builder

---
+++ src/Types/TR1/Misc/TR1CisternPlantsBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRImageControl.Packing;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1CisternPlantsBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        // This disables the animation on the underwater plant, by resetting it to
+        // only have one texture.
+        TR1Level cistern = _control1.Read($@"Resources\{TR1LevelNames.CISTERN}");
+        TRSpriteSequence plant = cistern.Sprites[TR1Type.Plant2];
+        plant.Textures = new() { plant.Textures[0] };
+
+        TR1TexturePacker packer = new(cistern);
+        TRTextileRegion region = packer.GetSpriteRegions(plant).Values.First().First();
+
+        ResetLevel(cistern, 1);
+
+        packer = new(cistern);
+        packer.AddRectangle(region);
+        packer.Pack(true);
+
+        cistern.Sprites[TR1Type.Plant2] = plant;
+
+        InjectionData data = InjectionData.Create(cistern, InjectionType.DisableAnimSprite, "cistern_plants");
+        return new() { data };
+    }
+}
