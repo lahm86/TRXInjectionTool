ef083d982ae1cc343b76eb5efbe538739c5bd006
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-11-28 21:05:41 +0000

    Fix dry shark room in Wreck

---
+++ src/Types/TR2/FD/TR2WreckFDBuilder.cs
@@ -0,0 +1,30 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.FD;
+
+public class TR2WreckFDBuilder : FDBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR2Level wreck = _control2.Read($"Resources/{TR2LevelNames.DORIA}");
+        InjectionData data = InjectionData.Create(TRGameVersion.TR2, InjectionType.FDFix, "wreck_fd");
+
+        // Flood room 98 where the unreachable shark is.
+        data.FloorEdits.Add(new()
+        {
+            RoomIndex = 98,
+            Fixes = new()
+            {
+                new FDRoomProperties
+                {
+                    Flags = wreck.Rooms[98].Flags | TRRoomFlag.Water,
+                }
+            }
+        });
+
+        return new() { data };
+    }
+}
