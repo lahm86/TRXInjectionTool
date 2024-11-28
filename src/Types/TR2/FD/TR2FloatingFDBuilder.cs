674affe741f5be8ba3ca262a3fd3533e41eb26c5
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-11-28 20:07:14 +0000

    Fix Floating Islands door

---
+++ src/Types/TR2/FD/TR2FloatingFDBuilder.cs
@@ -0,0 +1,21 @@
+﻿using TRLevelControl;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.FD;
+
+public class TR2FloatingFDBuilder : FDBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR2Level floating = _control2.Read($"Resources/{TR2LevelNames.FLOATER}");
+        InjectionData data = InjectionData.Create(TRGameVersion.TR2, InjectionType.FDFix, "floating_fd");
+
+        // Rotate and shift the door that leads to room 86, otherwise there is an invisible wall.
+        floating.Entities[72].X += TRConsts.Step4;
+        data.ItemEdits.Add(ItemBuilder.SetAngle(floating, 72, 16384));
+
+        return new() { data };
+    }
+}
