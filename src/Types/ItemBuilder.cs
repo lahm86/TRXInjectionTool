b6428170a695416810b155b7e77c57d3768910c5
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-11-28 19:27:10 +0000

    Fix Ice Palace secret door

--- src/Types/ItemBuilder.cs
+++ src/Types/ItemBuilder.cs
@@ -16,4 +16,22 @@ public abstract class ItemBuilder : InjectionBuilder
             Item = item,
         };
     }
+
+    public static TRItemEdit SetAngle(TR2Level level, short itemIndex, short angle)
+    {
+        // Convert to a TR1Entity
+        TR2Entity item = level.Entities[itemIndex];
+        return new()
+        {
+            Index = itemIndex,
+            Item = new()
+            {
+                Angle = angle,
+                X = item.X,
+                Y = item.Y,
+                Z = item.Z,
+                Room = item.Room,
+            },
+        };
+    }
 }
---
+++ src/Types/TR2/FD/TR2IcePalaceFDBuilder.cs
@@ -0,0 +1,22 @@
+﻿using TRLevelControl;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.FD;
+
+public class TR2IcePalaceFDBuilder : FDBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR2Level palace = _control2.Read($"Resources/{TR2LevelNames.CHICKEN}");
+        InjectionData data = InjectionData.Create(TRGameVersion.TR2, InjectionType.FDFix, "palace_fd");
+
+        // Rotate and shift the door that leads to the Jade secret, otherwise there is an invisible wall.
+        // Although this is an item shift, it's included in FD as it's closest to that in terms of config setting.
+        palace.Entities[143].X += TRConsts.Step4;
+        data.ItemEdits.Add(ItemBuilder.SetAngle(palace, 143, 16384));
+
+        return new() { data };
+    }
+}
