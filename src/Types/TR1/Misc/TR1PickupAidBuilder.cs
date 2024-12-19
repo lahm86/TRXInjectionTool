54ba19ac61403b5857192e63e6f4f8d55f1ecd7e
lahm86 <33758420+lahm86@users.noreply.github.com>
2024-12-19 19:36:53 +0000

    Create TR1 pickup highlight builder

---
+++ src/Types/TR1/Misc/TR1PickupAidBuilder.cs
@@ -0,0 +1,37 @@
+﻿using TRImageControl.Packing;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1PickupAidBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level midas = _control1.Read($"Resources/{TR1LevelNames.MIDAS}");
+        TRSpriteSequence twinkle = midas.Sprites[TR1Type.Sparkles_S_H];
+
+        TR1TexturePacker packer = new(midas);
+        IEnumerable<TRTextileRegion> regions = packer.GetSpriteRegions(twinkle)
+            .Values.SelectMany(r => r);
+
+        ResetLevel(midas, 1);
+
+        packer = new(midas);
+        packer.AddRectangles(regions);
+        packer.Pack(true);
+
+        foreach (TRSpriteTexture texture in twinkle.Textures)
+        {
+            texture.Alignment.Left -= 40;
+            texture.Alignment.Right += 40;
+            texture.Alignment.Top -= 40;
+        }
+
+        midas.Sprites[TR1Type.Unused02] = twinkle;
+
+        InjectionData data = InjectionData.Create(midas, InjectionType.General, "pickup_aid");
+        return new() { data };
+    }
+}
