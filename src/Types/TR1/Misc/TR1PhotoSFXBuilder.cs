c2e5be10bf12c6c41f871795d517ce304cecec0b
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-09 14:02:39 +0000

    Create TR1 photo SFX builder

---
+++ src/Types/TR1/Misc/TR1PhotoSFXBuilder.cs
@@ -0,0 +1,20 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1PhotoSFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR1Level caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
+        TR1SoundEffect fx = caves.SoundEffects[TR1SFX.MenuChoose];
+        ResetLevel(caves);
+
+        caves.SoundEffects[TR1SFX.MenuChoose] = fx;
+
+        InjectionData data = InjectionData.Create(caves, InjectionType.General, "photo");
+        return new() { data };
+    }
+}
