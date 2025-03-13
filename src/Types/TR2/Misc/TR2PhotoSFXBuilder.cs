340de8f54330b7920e648644d80cbf88fcf7c2ec
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-03-13 21:41:17 +0000

    Create TR2 photo SFX builder
    This adds the camera SFX for TR2 cutscenes.

---
+++ src/Types/TR2/Misc/TR2PhotoSFXBuilder.cs
@@ -0,0 +1,20 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.Misc;
+
+public class TR2PhotoSFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR2Level wall = _control2.Read($"Resources/{TR2LevelNames.GW}");
+        TR2SoundEffect fx = wall.SoundEffects[TR2SFX.MenuLaraHome];
+        ResetLevel(wall);
+
+        wall.SoundEffects[TR2SFX.MenuLaraHome] = fx;
+
+        InjectionData data = InjectionData.Create(wall, InjectionType.General, "photo");
+        return new() { data };
+    }
+}
