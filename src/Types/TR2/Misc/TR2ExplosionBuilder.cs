0685aeba66c5919dfa74875cdeb61002bd3cb79f
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-04-28 20:29:39 +0100

    Add explosion sprites to HSH

---
+++ src/Types/TR2/Misc/TR2ExplosionBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using TRImageControl;
+using TRImageControl.Packing;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.Misc;
+
+public class TR2ExplosionBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        TR2Level wall = _control2.Read($"Resources/{TR2LevelNames.GW}");
+        TRSpriteSequence explosion = wall.Sprites[TR2Type.Explosion_S_H];
+
+        TR2TexturePacker packer = new(wall);
+        IEnumerable<TRTextileRegion> regions = packer.GetSpriteRegions(explosion)
+            .Values.SelectMany(r => r);
+
+        List<Color> basePalette = new(wall.Palette.Select(c => c.ToTR1Color()));
+        ResetLevel(wall, 1);
+
+        packer = new(wall);
+        packer.AddRectangles(regions);
+        packer.Pack(true);
+
+        wall.Sprites[TR2Type.Explosion_S_H] = explosion;
+        GenerateImages8(wall, basePalette);
+
+        InjectionData data = InjectionData.Create(wall, InjectionType.General, "explosion");
+        return new() { data };
+    }
+}
