29caefee5d920f4e7ec426fc8b7daeab52977b38
Marcin Kurczewski <dash@wind.garden>
2025-06-23 21:42:37 +0200

    Fix missing zipline sound in Home Sweet Home

---
+++ src/Types/TR2/SFX/TR2HSHSFXBuilder.cs
@@ -0,0 +1,32 @@
+using TRDataControl;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.SFX;
+
+public class TR2HSHSFXBuilder : InjectionBuilder
+{
+    public override string ID => "house_sfx";
+
+    public override List<InjectionData> Build()
+    {
+        TR2Level wall = _control2.Read($"Resources/{TR2LevelNames.GW}");
+        TR2SFX[] soundIDs = new[]
+        {
+            TR2SFX.DeathSlideGrab,
+            TR2SFX.DeathSlideGo,
+            TR2SFX.DeathSlideStop,
+        };
+
+        TRModel lara = wall.Models[TR2Type.Lara];
+        TRDictionary<TR2SFX, TR2SoundEffect> copiedSounds = new();
+        soundIDs.ToList().ForEach(s => copiedSounds[s] = wall.SoundEffects[s]);
+
+        ResetLevel(wall);
+        wall.SoundEffects = copiedSounds;
+
+        InjectionData data = InjectionData.Create(wall, InjectionType.General, ID);
+        return new() { data };
+    }
+}
