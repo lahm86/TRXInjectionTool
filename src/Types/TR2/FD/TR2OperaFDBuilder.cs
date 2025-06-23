a09d0b9e2620289f9b07c6829f162f017ace1534
Marcin Kurczewski <dash@wind.garden>
2025-06-23 16:10:05 +0200

    Fix room 136 in Opera House not having water

--- src/Types/TR2/FD/TR2OperaFDBuilder.cs
+++ src/Types/TR2/FD/TR2OperaFDBuilder.cs
@@ -9,9 +9,23 @@ public class TR2OperaFDBuilder : FDBuilder
 {
     public override List<InjectionData> Build()
     {
+        TR2Level opera = _control2.Read($"Resources/{TR2LevelNames.OPERA}");
         InjectionData data = InjectionData.Create(TRGameVersion.TR2, InjectionType.FDFix, "opera_fd");
         CreateDefaultTests(data, TR2LevelNames.OPERA);
         data.FloorEdits.Add(FixLooseBoardTrigger());
+
+        // Flood room 136 near the airplane.
+        data.FloorEdits.Add(new()
+        {
+            RoomIndex = 136,
+            Fixes = new()
+            {
+                new FDRoomProperties
+                {
+                    Flags = opera.Rooms[136].Flags | TRRoomFlag.Water,
+                }
+            }
+        });
 
         return new() { data };
     }
