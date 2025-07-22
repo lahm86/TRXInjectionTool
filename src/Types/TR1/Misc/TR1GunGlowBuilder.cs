85026999fe8526f0c88d82821d97b58b8b2ec1bb
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-07-22 19:42:18 +0100

    Create TR1 gun glow builder

---
+++ src/Types/TR1/Misc/TR1GunGlowBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRImageControl.Packing;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Misc;
+
+public class TR1GunGlowBuilder : InjectionBuilder
+{
+    public override string ID => "gun_glow";
+
+    public override List<InjectionData> Build()
+    {
+        var wall = _control2.Read($"Resources/{TR2LevelNames.GW}");
+        var sequence = wall.Sprites[TR2Type.Glow_S_H];
+        var packer2 = new TR2TexturePacker(wall);
+        var regions = packer2.GetSpriteRegions(sequence)
+            .Values.SelectMany(r => r);
+
+        var caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
+        ResetLevel(caves, 1);
+
+        var packer1 = new TR1TexturePacker(caves);
+        packer1.AddRectangles(regions);
+        packer1.Pack(true);
+
+        caves.Sprites[TR1Type.Glow_S_H] = sequence;
+
+        var data = InjectionData.Create(caves, InjectionType.General, ID);
+        return new() { data };
+    }
+}
