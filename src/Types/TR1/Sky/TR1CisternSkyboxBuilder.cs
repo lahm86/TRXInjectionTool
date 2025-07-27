648a6feabfd37680b1f6d83e4807d2e017496f49
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-07-27 20:19:04 +0100

    Create Cistern skybox builder
    This creates a separate injection for the skybox in The Cistern in order to also darken off the area above Lara at the start.

---
+++ src/Types/TR1/Sky/TR1CisternSkyboxBuilder.cs
@@ -0,0 +1,54 @@
+﻿using TRLevelControl.Helpers;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.Sky;
+
+public class TR1CisternSkyboxBuilder : TR1ColosseumSkyboxBuilder
+{
+    public override string ID => "cistern_skybox";
+
+    public override List<InjectionData> Build()
+    {
+        var level = CreateBaseLevel();
+        var data = InjectionData.Create(level, InjectionType.Skybox, ID);
+        CreateDefaultTests(data, TR1LevelNames.CISTERN);
+        AdjustMidasGap(data);
+        return new() { data };
+    }
+
+    private static void AdjustMidasGap(InjectionData data)
+    {
+        var verts = new List<List<ushort>>
+        {
+            new() { 6, 7, 1, 0 },
+            new() { 7, 10, 2, 1 },
+            new() { 10, 11, 9, 2 },
+            new() { 11, 8, 4, 9 },
+            new() { 8, 5, 3, 4 },
+            new() { 5, 6, 0, 3 },
+            new() { 8, 7, 6, 5 },
+            new() { 11, 10, 7, 8 }
+        };
+        data.RoomEdits.AddRange(verts.Select(v => new TRRoomTextureCreate
+        {
+            RoomIndex = 138,
+            FaceType = TRMeshFaceType.TexturedQuad,
+            SourceRoom = 137,
+            SourceIndex = 2,
+            Vertices = v,
+        }));
+
+        var cistern = _control1.Read($"Resources/{TR1LevelNames.CISTERN}");
+        for (ushort i = 0; i < 12; i++)
+        {
+            data.RoomEdits.Add(new TRRoomVertexMove
+            {
+                RoomIndex = 138,
+                VertexChange = new(),
+                VertexIndex = i,
+                ShadeChange = (short)(8192 - cistern.Rooms[138].Mesh.Vertices[i].Lighting),
+            });
+        }
+    }
+}
--- src/Types/TR1/Sky/TR1ColosseumSkyboxBuilder.cs
+++ src/Types/TR1/Sky/TR1ColosseumSkyboxBuilder.cs
@@ -1,54 +1,62 @@
-﻿using TRImageControl.Packing;
+﻿using System.Drawing;
 using TRImageControl;
+using TRImageControl.Packing;
 using TRLevelControl.Helpers;
 using TRLevelControl.Model;
 using TRXInjectionTool.Control;
-using System.Drawing;
 
 namespace TRXInjectionTool.Types.TR1.Sky;
 
 public class TR1ColosseumSkyboxBuilder : InjectionBuilder
 {
-    private static readonly Dictionary<string, string> _imageIDs = new()
+    protected static readonly Dictionary<string, string> _imageIDs = new()
     {
         ["6063df8273ce88d50da6c6b20fd48802"] = "Resources/TR1/Colosseum/sky1.png",
         ["74827eb9fffd8d7cac2ce1807bd37bce"] = "Resources/TR1/Colosseum/sky2.png",
     };
 
+    public override string ID => "colosseum_skybox";
+
     public override List<InjectionData> Build()
     {
-        TR1Level caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
-        TR2Level gold = _control2.Read($"Resources/{TR2LevelNames.FOOLGOLD}");
+        var level = CreateBaseLevel();
+        var data = InjectionData.Create(level, InjectionType.Skybox, ID);
+        return new() { data };
+    }
+
+    protected static TR1Level CreateBaseLevel()
+    {
+        var caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
+        var gold = _control2.Read($"Resources/{TR2LevelNames.FOOLGOLD}");
 
         ResetLevel(caves, 1);
 
-        TRModel sky = gold.Models[TR2Type.Skybox_H];
+        var sky = gold.Models[TR2Type.Skybox_H];
         caves.Models[TR1Type.Unused01] = sky;
         caves.Palette = InitialisePalette8(sky, gold.Palette16);
 
-        Color topColour = Color.FromArgb(24, 16, 48);
+        var topColour = Color.FromArgb(24, 16, 48);
 
         PackTextures(caves, gold, sky, _imageIDs);
 
         // Replace the flat faces at the top of the mesh with a texture.
-        TR1TexturePacker packer = new(caves);
-        TRImage img = new(16, 16);
+        var packer = new TR1TexturePacker(caves);
+        var img = new TRImage(16, 16);
         img.Write((c, x, y) => topColour);
-        TRTextileRegion region = new(new()
+        var region = new TRTextileRegion(new()
         {
             Texture = new TRObjectTexture(0, 0, 16, 16),
         }, img);
         packer.AddRectangle(region);
         packer.Pack(true);
 
-        List<TRMeshFace> topTriangles = sky.Meshes[0].ColouredTriangles.Where(f => f.Vertices.Contains(32)).ToList();
+        var topTriangles = sky.Meshes[0].ColouredTriangles.Where(f => f.Vertices.Contains(32)).ToList();
         sky.Meshes[0].TexturedTriangles.AddRange(topTriangles);
         sky.Meshes[0].ColouredTriangles.RemoveAll(topTriangles.Contains);
         topTriangles.ForEach(t => t.Texture = (ushort)caves.ObjectTextures.Count);
 
         caves.ObjectTextures.Add(region.Segments.First().Texture as TRObjectTexture);
 
-        InjectionData data = InjectionData.Create(caves, InjectionType.Skybox, "colosseum_skybox");
-        return new() { data };
+        return caves;
     }
 }
