8eb2ac0bf188f8bf692ba77a2a2c1630780e07ae
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-11-03 09:19:25 +0000

    Fix DA cutscene end frame
    Avoids Lara jumping back to the start of her final animation in the DA cutscene.

---
+++ src/Types/TR2/Lara/TR2CutsceneBuilder.cs
@@ -0,0 +1,31 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.Lara;
+
+public class TR2CutsceneBuilder : InjectionBuilder
+{
+    public override string ID => "tr2_cutscenes";
+
+    public override List<InjectionData> Build()
+    {
+        return
+        [
+            CreateCut3Data(),
+        ];
+    }
+
+    private static InjectionData CreateCut3Data()
+    {
+        var cut = _control2.Read($"Resources/{TR2LevelNames.DA_CUT}");
+        var lara = cut.Models[TR2Type.Lara];
+        ResetLevel(cut);
+        cut.Models[TR2Type.Lara] = lara;
+
+        var endAnim = lara.Animations[^1];
+        endAnim.NextFrame = (ushort)endAnim.FrameEnd;
+
+        return InjectionData.Create(cut, InjectionType.General, "cut3_setup", true);
+    }
+}
