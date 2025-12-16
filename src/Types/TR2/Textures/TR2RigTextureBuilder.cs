40af0948cd20e42de2cf0c49733e8d13f1e9ab11
lahm86 <33758420+lahm86@users.noreply.github.com>
2025-12-16 12:45:24 +0000

    Fix missing tanks in Offshore Rig

--- src/Types/TR2/Textures/TR2RigTextureBuilder.cs
+++ src/Types/TR2/Textures/TR2RigTextureBuilder.cs
@@ -28,21 +28,22 @@ public partial class TR2RigTextureBuilder : TextureBuilder
         FixWheelDoor(data, TR2LevelNames.RIG);
         FixSlidingOffshoreDoor(data, TR2LevelNames.RIG);
         FixPlaneStatics(level, data);
+        FixOxygenTanks(level, data);
 
-        return new() { data };
+        return [data];
     }
 
     private static List<TRRoomTextureMove> CreateShifts(TR2Level level)
     {
-        return new()
-        {
+        return
+        [
             new()
             {
                 RoomIndex = 35,
                 FaceType = TRMeshFaceType.TexturedQuad,
                 TargetIndex = 42,
-                VertexRemap = new()
-                {
+                VertexRemap =
+                [
                     new()
                     {
                         Index = 0,
@@ -53,15 +54,15 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                         Index = 1,
                         NewVertexIndex = level.Rooms[35].Mesh.Rectangles[28].Vertices[0],
                     }
-                }
+                ]
             },
             new()
             {
                 RoomIndex = 81,
                 FaceType = TRMeshFaceType.TexturedQuad,
                 TargetIndex = 33,
-                VertexRemap = new()
-                {
+                VertexRemap =
+                [
                     new()
                     {
                         Index = 0,
@@ -72,28 +73,28 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                         Index = 1,
                         NewVertexIndex = level.Rooms[81].Mesh.Rectangles[23].Vertices[0],
                     }
-                }
+                ]
             },
-        };
+        ];
     }
 
     private static List<TRRoomTextureCreate> CreateFillers(TR2Level level)
     {
-        return new()
-        {
+        return
+        [
             new()
             {
                 RoomIndex = 35,
                 FaceType = TRMeshFaceType.TexturedQuad,
                 SourceRoom = 35,
                 SourceIndex = 56,
-                Vertices = new()
-                {
+                Vertices =
+                [
                     level.Rooms[35].Mesh.Rectangles[56].Vertices[1],
                     level.Rooms[35].Mesh.Rectangles[30].Vertices[0],
                     level.Rooms[35].Mesh.Rectangles[30].Vertices[3],
                     level.Rooms[35].Mesh.Rectangles[56].Vertices[2],
-                }
+                ]
             },
             new()
             {
@@ -101,34 +102,34 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                 FaceType = TRMeshFaceType.TexturedQuad,
                 SourceRoom = 81,
                 SourceIndex = 43,
-                Vertices = new()
-                {
+                Vertices =
+                [
                     level.Rooms[81].Mesh.Rectangles[43].Vertices[1],
                     level.Rooms[81].Mesh.Rectangles[25].Vertices[0],
                     level.Rooms[81].Mesh.Rectangles[25].Vertices[3],
                     level.Rooms[81].Mesh.Rectangles[43].Vertices[2],
-                }
+                ]
             },
-        };
+        ];
     }
 
     private static List<TRRoomTextureReface> CreateRefacings(TR2Level level)
     {
-        return new()
-        {
+        return
+        [
             Reface(level, 11, TRMeshFaceType.TexturedQuad, TRMeshFaceType.TexturedQuad, 1622, 137),
             Reface(level, 33, TRMeshFaceType.TexturedTriangle, TRMeshFaceType.TexturedTriangle, 1736, 0),
             Reface(level, 35, TRMeshFaceType.TexturedQuad, TRMeshFaceType.TexturedQuad, 1702, 42),
             Reface(level, 81, TRMeshFaceType.TexturedQuad, TRMeshFaceType.TexturedQuad, 1702, 33),
-        };
+        ];
     }
 
     private static List<TRRoomTextureRotate> CreateRotations()
     {
-        return new()
-        {
+        return
+        [
             Rotate(33, TRMeshFaceType.TexturedTriangle, 0, 2),
-        };
+        ];
     }
 
     private static void FixThinWall(TR2Level level, InjectionData data)
@@ -154,8 +155,8 @@ public partial class TR2RigTextureBuilder : TextureBuilder
             FaceType = TRMeshFaceType.TexturedQuad,
             RoomIndex = 11,
             TargetIndex = 83,
-            VertexRemap = new()
-            {
+            VertexRemap =
+            [
                 new()
                 {
                     Index = 0,
@@ -166,15 +167,15 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                     Index = 1,
                     NewVertexIndex = level.Rooms[11].Mesh.Rectangles[135].Vertices[2],
                 }
-            }
+            ]
         });
         data.RoomEdits.Add(new TRRoomTextureMove
         {
             FaceType = TRMeshFaceType.TexturedQuad,
             RoomIndex = 11,
             TargetIndex = 135,
-            VertexRemap = new()
-            {
+            VertexRemap =
+            [
                 new()
                 {
                     Index = 0,
@@ -185,7 +186,7 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                     Index = 3,
                     NewVertexIndex = (ushort)level.Rooms[11].Mesh.Vertices.Count,
                 }
-            }
+            ]
         });
 
 
@@ -196,24 +197,24 @@ public partial class TR2RigTextureBuilder : TextureBuilder
             RoomIndex = 11,
             X = 2,
             Z = 16,
-            Fixes = new()
-            {
+            Fixes =
+            [
                 new FDSectorOverwrite
                 {
                     Sector = sector,
                 },
                 new FDTrigCreateFix
                 {
-                    Entries = new()
-                    {
+                    Entries =
+                    [
                         new FDSlantEntry
                         {
                             Type = FDSlantType.Ceiling,
                             ZSlant = 3,
                         }
-                    }
+                    ]
                 }
-            }
+            ]
         });
     }
 
@@ -225,46 +226,46 @@ public partial class TR2RigTextureBuilder : TextureBuilder
 
     private static void FixPlaneStatics(TR2Level level, InjectionData data)
     {
-        data.RoomEdits.Add(CreateQuadShift(92, 4, new()
-        {
+        data.RoomEdits.Add(CreateQuadShift(92, 4,
+        [
             new(0, level.Rooms[92].Mesh.Rectangles[4].Vertices[3]),
             new(1, level.Rooms[92].Mesh.Rectangles[4].Vertices[2]),
             new(2, level.Rooms[92].Mesh.Rectangles[4].Vertices[1]),
             new(3, level.Rooms[92].Mesh.Rectangles[4].Vertices[0]),
-        }));
-        data.RoomEdits.Add(CreateQuadShift(92, 27, new()
-        {
+        ]));
+        data.RoomEdits.Add(CreateQuadShift(92, 27,
+        [
             new(0, level.Rooms[92].Mesh.Rectangles[27].Vertices[3]),
             new(1, level.Rooms[92].Mesh.Rectangles[27].Vertices[2]),
             new(2, level.Rooms[92].Mesh.Rectangles[27].Vertices[1]),
             new(3, level.Rooms[92].Mesh.Rectangles[27].Vertices[0]),
-        }));
+        ]));
 
         foreach (var face in new[] { 27, 5, 30, 7 })
         {
-            data.RoomEdits.Add(CreateQuadShift(95, (short)face, new()
-            {
+            data.RoomEdits.Add(CreateQuadShift(95, (short)face,
+            [
                 new(0, level.Rooms[95].Mesh.Rectangles[20].Vertices[1]),
                 new(1, level.Rooms[95].Mesh.Rectangles[20].Vertices[0]),
                 new(2, level.Rooms[95].Mesh.Rectangles[20].Vertices[3]),
                 new(3, level.Rooms[95].Mesh.Rectangles[20].Vertices[2]),
-            }));
+            ]));
         }
 
-        data.RoomEdits.Add(CreateQuadShift(2, 212, new()
-        {
+        data.RoomEdits.Add(CreateQuadShift(2, 212,
+        [
             new(0, level.Rooms[2].Mesh.Rectangles[211].Vertices[1]),
             new(1, level.Rooms[2].Mesh.Rectangles[211].Vertices[0]),
             new(2, level.Rooms[2].Mesh.Rectangles[211].Vertices[3]),
             new(3, level.Rooms[2].Mesh.Rectangles[211].Vertices[2]),
-        }));
-        data.RoomEdits.Add(CreateQuadShift(96, 33, new()
-        {
+        ]));
+        data.RoomEdits.Add(CreateQuadShift(96, 33,
+        [
             new(0, level.Rooms[96].Mesh.Rectangles[31].Vertices[1]),
             new(1, level.Rooms[96].Mesh.Rectangles[31].Vertices[0]),
             new(2, level.Rooms[96].Mesh.Rectangles[31].Vertices[3]),
             new(3, level.Rooms[96].Mesh.Rectangles[31].Vertices[2]),
-        }));
+        ]));
 
         data.MeshEdits.AddRange(new[] { 17, 18 }.Select(type =>
             new TRMeshEdit
@@ -282,5 +283,23 @@ public partial class TR2RigTextureBuilder : TextureBuilder
                         }
                     }).ToList(),            
             }));
+    }
+
+    private static void FixOxygenTanks(TR2Level level, InjectionData data)
+    {
+        var mesh = level.Rooms[34].StaticMeshes[^1];
+        data.RoomEdits.Add(new TRRoomStatic3DCreate
+        {
+            ID = 33,            
+            RoomIndex = level.Rooms[34].AlternateRoom,
+            StaticMesh = new()
+            {
+                X = mesh.X,
+                Y = mesh.Y,
+                Z = mesh.Z,
+                Angle = mesh.Angle,
+                Intensity = mesh.Intensity1,
+            }
+        });
     }
 }
