0488f9153b11c0f196c5395d3a28b58ad958da99
Dash <dash@bad.pink>
2026-01-01 18:25:16 +0100

    Remove Gym music trigger (moving to lua)

--- src/Types/TR2/FD/TR2GymFDBuilder.cs
+++ src/Types/TR2/FD/TR2GymFDBuilder.cs
@@ -15,8 +15,18 @@ public class TR2GymFDBuilder : FDBuilder
         var data = InjectionData.Create(TRGameVersion.TR2, InjectionType.FDFix, ID);
         CreateDefaultTests(data, TR2LevelNames.ASSAULT);
         data.FloorEdits.AddRange(FixZipTriggers(level));
+        data.FloorEdits.Add(RemoveInitialSpeechTrigger(level));
 
         return new() { data };
+    }
+
+    private static TRFloorDataEdit RemoveInitialSpeechTrigger(TR2Level level)
+    {
+        // Remove the opening voice line for Lara: for LostArtefacts/TRX#2822,
+        // we play different tracks conditionally via LUA.
+        var trig = GetTrigger(level, 16, 4, 6);
+        trig.Actions.RemoveAll(a => a.Action == FDTrigAction.PlaySoundtrack);
+        return MakeTrigger(level, 16, 4, 6, trig);
     }
 
     private static IEnumerable<TRFloorDataEdit> FixZipTriggers(TR2Level level)
