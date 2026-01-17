9130c2227191615aac0b536a4361d54ddf50961a
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-01-17 11:45:30 +0000

    Adjust cutscene actor end frames
    This ensures all cutscene actors stop animating on their final frame.

--- src/Types/TR3/Lara/TR3CutsceneBuilder.cs
+++ src/Types/TR3/Lara/TR3CutsceneBuilder.cs
@@ -7,6 +7,13 @@ namespace TRXInjectionTool.Types.TR3.Lara;
 
 public class TR3CutsceneBuilder : InjectionBuilder
 {
+    private static readonly List<TR3Type> _actors =
+    [
+        TR3Type.Lara, TR3Type.CutsceneActor1, TR3Type.CutsceneActor2, TR3Type.CutsceneActor3,
+        TR3Type.CutsceneActor4, TR3Type.CutsceneActor5, TR3Type.CutsceneActor6,
+        TR3Type.CutsceneActor7, TR3Type.CutsceneActor8, TR3Type.CutsceneActor9,
+    ];
+
     public override string ID => "tr3_cutscenes";
 
     public override List<InjectionData> Build()
@@ -14,18 +21,52 @@ public class TR3CutsceneBuilder : InjectionBuilder
         return
         [
             CreateCut6Data(),
+            CreateCut9Data(),
+            CreateCut1Data(),
+            CreateCut4Data(),
             CreateCut2Data(),
+            CreateCut5Data(),
+            CreateCut11Data(),
+            CreateCut7Data(),
+            CreateCut8Data(),
+            CreateCut3Data(),
+            CreateCut12Data(),
         ];
+    }
+
+    private static InjectionData CreateBaseData(string levelName)
+    {
+        var level = _control3.Read($"Resources/TR3/{levelName}");
+        var actors = _actors.Where(level.Models.ContainsKey).ToArray();
+        foreach (var type in actors)
+        {
+            var model = level.Models[type];
+            var endAnim = model.Animations[^1];
+            endAnim.NextAnimation = (ushort)(model.Animations.Count - 1);
+            endAnim.NextFrame = (ushort)endAnim.FrameEnd;
+            model.MeshTrees.Clear();
+            model.Meshes.Clear();
+        }
+
+        CreateModelLevel(level, actors);
+        level.SoundEffects.Clear();
+        level.Images16.Clear();
+        level.Images8.Clear();
+
+        return InjectionData.Create(level, InjectionType.General,
+            $"{Path.GetFileNameWithoutExtension(levelName).ToLower()}_setup");
     }
 
     private static InjectionData CreateCommonCutData(string levelName, short laraRot)
     {
         var cut = _control3.Read($"Resources/TR3/{levelName}");
-        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General,
-            $"{Path.GetFileNameWithoutExtension(levelName).ToLower()}_setup");
+        var data = CreateBaseData(levelName);
 
         var laraIdx = cut.Entities.FindIndex(e => e.TypeID == TR3Type.Lara);
-        data.ItemPosEdits.Add(ItemBuilder.SetAngle(cut, (short)laraIdx, laraRot));
+        if (cut.Entities[laraIdx].Angle != laraRot)
+        {
+            data.ItemPosEdits.Add(ItemBuilder.SetAngle(cut, (short)laraIdx, laraRot));
+        }
 
         return data;
     }
@@ -62,6 +103,33 @@ public class TR3CutsceneBuilder : InjectionBuilder
         return data;
     }
 
+    private static InjectionData CreateCut9Data()
+        => CreateCommonCutData(TR3LevelNames.RUINS_CUT, 16384);
+
+    private static InjectionData CreateCut1Data()
+        => CreateCommonCutData(TR3LevelNames.COASTAL_CUT, 16384);
+
+    private static InjectionData CreateCut4Data()
+        => CreateCommonCutData(TR3LevelNames.CRASH_CUT, 16384);
+
     private static InjectionData CreateCut2Data()
         => CreateCommonCutData(TR3LevelNames.THAMES_CUT, -16384);
+
+    private static InjectionData CreateCut5Data()
+        => CreateCommonCutData(TR3LevelNames.ALDWYCH_CUT, 16384);
+
+    private static InjectionData CreateCut11Data()
+        => CreateCommonCutData(TR3LevelNames.LUDS_CUT, 16384);
+
+    private static InjectionData CreateCut7Data()
+        => CreateCommonCutData(TR3LevelNames.NEVADA_CUT, 16384);
+
+    private static InjectionData CreateCut8Data()
+        => CreateCommonCutData(TR3LevelNames.HSC_CUT, 16384);
+
+    private static InjectionData CreateCut3Data()
+        => CreateCommonCutData(TR3LevelNames.ANTARC_CUT, 16384);
+
+    private static InjectionData CreateCut12Data()
+        => CreateCommonCutData(TR3LevelNames.TINNOS_CUT, 16384);
 }
