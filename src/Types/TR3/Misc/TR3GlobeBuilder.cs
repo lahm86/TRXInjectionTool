6dce932b1908f44d4d12a5d29948c1fdcdb93f8f
Dash <dash@bad.pink>
2026-01-18 12:03:05 +0100

    Add TR3 Globe builder

---
+++ src/Types/TR3/Misc/TR3GlobeBuilder.cs
@@ -0,0 +1,15 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Misc;
+
+public class TR3GlobeBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var srcLevel = _control3.Read("Resources/TR3/TITLE.TR2");
+        CreateModelLevel(srcLevel, TR3Type.Globe_M_H);
+        var data = InjectionData.Create(srcLevel, InjectionType.General, "globe_model");
+        return [data];
+    }
+}
