bbe7c82dc2e93ad5d99f03b4a9502432c9e6a1ac
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-03-12 12:30:28 +0000

    Fix Willard's Lair helicopter
    This updates the helicopter in Willard's Lair to ensure it's in the correct starting room.

---
+++ src/Types/TR3/Misc/TR3WillsDenHeliBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Misc;
+
+public class TR3WillsDenHeliBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.LAIR}");
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, "willsden_heli");
+
+        // The helicopter cannot update its room because the original position means portal traversal is
+        // impossible. Force the room num instead and disable an out-of-bounds portal in the opposite
+        // direction to prevent it reverting to the original room.
+        var heliIdx = (short)level.Entities.FindIndex(e => e.TypeID == TR3Type.Animating4);
+        Debug.Assert(heliIdx != -1);
+        data.ItemPosEdits.Add(ItemBuilder.MoveToRoom(level, heliIdx, 10));
+        data.FloorEdits.Add(new()
+        {
+            RoomIndex = 15,
+            X = 7,
+            Z = 5,
+            Fixes = [new FDPortalOverwrite()],
+        });
+
+        return [data];
+    }
+}
