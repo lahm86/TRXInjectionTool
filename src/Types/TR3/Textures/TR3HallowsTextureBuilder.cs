c4b2a33371f3cdd95eb712cccc495f4ce58196a6
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-03-18 19:53:12 +0000

    Fix missing Eye of Isis in All Hallows

---
+++ src/Types/TR3/Textures/TR3HallowsTextureBuilder.cs
@@ -0,0 +1,24 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3HallowsTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = CreateBaseData();
+        CreateDefaultTests(data, $"TR3/{TR3LevelNames.HALLOWS}");
+        return [data];
+    }
+
+    private static InjectionData CreateBaseData()
+    {
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.CITY}");
+        CreateModelLevel(level, TR3Type.EyeOfIsis_M_H);
+        level.SoundEffects.Clear();
+
+        return InjectionData.Create(level, InjectionType.TextureFix, "stpaul_textures");
+    }
+}
