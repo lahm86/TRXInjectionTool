e1e96720d9aeaf0756459c398313a0ac498cb173
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-03-24 10:17:38 +0000

    Add detonator dynamic lights
    This adds animation commands to the TR2 detonator to signal dynamic light
    output; the effect is not used in TR3. The TR2 WAD has been updated for builder
    convenience.

---
+++ src/Types/TR2/Misc/TR2DetonatorBuilder.cs
@@ -0,0 +1,38 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR2.Misc;
+
+public class TR2DetonatorBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var level = _control2.Read($"Resources/{TR2LevelNames.BARTOLI}");
+        var detonator = level.Models[TR2Type.DetonatorBox];
+        ResetLevel(level);
+        level.Models[TR2Type.DetonatorBox] = detonator;
+
+        var anim = detonator.Animations[0];
+        anim.Commands.Add(new TRFXCommand
+        {
+            FrameNumber = 76,
+            EffectID = (short)TR2FX.DynamicLightsOn,
+        });
+        anim.Commands.Add(new TRFXCommand
+        {
+            FrameNumber = 100,
+            EffectID = (short)TR2FX.DynamicLightsOff,
+        });
+
+        var data = InjectionData.Create(level, InjectionType.General, "detonator_lights", true);
+        data.Animations.Clear();
+        data.AnimFrames.Clear();
+        data.AnimChanges.Clear();
+        data.AnimDispatches.Clear();
+        data.Models.Clear();
+        
+        data.AnimCmdEdits.Add(CreateAnimCmdEdit(level, TR2Type.DetonatorBox, 0));
+        return [data];
+    }
+}
