09b73cdfe07bea4abbf1bb04fa748d87f0199632
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-03-28 15:21:18 +0000

    Fix London staircase mesh

---
+++ src/Types/TR3/Textures/TR3AldwychTextureBuilder.cs
@@ -0,0 +1,59 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3AldwychTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "aldwych_textures");
+        FixStaircaseMesh(data, 37);
+        return [data];
+    }
+
+    public static void FixStaircaseMesh(InjectionData data, uint meshIdx)
+    {
+        var right = new[] { 0, 1, 2, 9, 10, 15, 16 };
+        var left = new[] { 3, 4, 5, 11, 12, 17, 18 };
+        var top = new[] { 1, 2, 4, 5, 7, 8 };
+        var back = new[] { 2, 5, 7 };
+
+        TRVertex GetChange(int index)
+        {
+            var vtx = new TRVertex();
+            
+            if (right.Contains(index))
+            {
+                vtx.Z += 6;
+            }
+            else if (left.Contains(index))
+            {
+                vtx.Z -= 6;
+            }
+            
+            if (top.Contains(index))
+            {
+                vtx.Y -= 6;
+            }
+            if (back.Contains(index))
+            {
+                vtx.X -= 6;
+            }
+
+            return vtx;
+        }
+
+        data.MeshEdits.Add(new()
+        {
+            ModelID = (uint)TR3Type.SceneryBase + meshIdx,
+            EnforcedType = TRObjectType.Static3D,
+            VertexEdits = [.. Enumerable.Range(0, 21).Select(i => new TRVertexEdit
+            {
+                Index = (short)i,
+                Change = GetChange(i),
+            })],
+        });
+    }
+}
---
+++ src/Types/TR3/Textures/TR3LudsTextureBuilder.cs
@@ -0,0 +1,14 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3LudsTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "luds_textures");
+        TR3AldwychTextureBuilder.FixStaircaseMesh(data, 49);
+        return [data];
+    }
+}
