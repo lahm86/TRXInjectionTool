ed7f49ae31f3ccbadcdaef18e11266edcd7be834
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-03-28 14:38:39 +0000

    Fix Area 51 sliding door

---
+++ src/Types/TR3/Textures/TR3Area51TextureBuilder.cs
@@ -0,0 +1,34 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3Area51TextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "area51_textures");
+        FixSlidingDoor(data);
+        return [data];
+    }
+
+    private static void FixSlidingDoor(InjectionData data)
+    {
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.AREA51}");
+        var door = level.Models[TR3Type.Door7];
+        level.Models = new()
+        {
+            [TR3Type.Door7] = level.Models[TR3Type.Door7],
+        };
+
+        foreach (var frame in door.Animations.SelectMany(a => a.Frames))
+        {
+            frame.OffsetY += 30;
+            frame.Bounds.MinY += 30;
+            frame.Bounds.MaxY += 30;
+        }
+        data.FrameReplacements.AddRange(TRFrameReplacement.CreateFrom(level));
+    }
+}
