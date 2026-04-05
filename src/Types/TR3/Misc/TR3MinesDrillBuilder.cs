72d8c6faf10600933fd5faff2009a7ada4bcee85
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-04-05 21:24:57 +0100

    Fix RX-Tech drill mesh
    This adds a small hitbox to the mesh beside the drill bits in RX-Tech Mines.

---
+++ src/Types/TR3/Misc/TR3MinesDrillBuilder.cs
@@ -0,0 +1,29 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Misc;
+
+public class TR3MinesDrillBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        // If Lara hits a drillbit on the minecart, it's actually the static mesh on the
+        // wall that deals the damage. OG has a degenerate hitbox though, so adjust it to
+        // avoid it becoming non-collidable.
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.RXTECH}");
+        var mesh = level.StaticMeshes[TR3Type.SceneryBase + 34];
+        mesh.CollisionBox.MaxX++;
+        mesh.CollisionBox.MaxY++;
+        mesh.CollisionBox.MaxZ++;
+
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, "drill_collision");
+        data.StaticMeshEdits.Add(new()
+        {
+            TypeID = 34,
+            Mesh = mesh,
+        });
+
+        return [data];
+    }
+}
