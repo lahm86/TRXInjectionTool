2cf88958dfbf2b607bf2c6e2f65c15df71338ce0
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-04-08 09:58:17 +0100

    Add TR3 flamethrower SFX
    This restores the missing flamethrower blast SFX in TR3.

---
+++ src/Types/TR3/SFX/TR3FlamethrowerSFXBuilder.cs
@@ -0,0 +1,17 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.SFX;
+
+public class TR3FlamethrowerSFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.ANTARC}");
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, "flamethrower_sfx");
+        data.SFX.Add(TRSFXData.Create(TR3SFX.FlameThrowerLoop, level.SoundEffects[TR3SFX.FlameThrowerLoop]));
+        return [data];
+    }
+}
