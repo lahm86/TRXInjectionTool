1e2ec7d1ade869205b692c133ca978f800121617
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-10 19:50:27 +0100

    Fix Tinnos push buttons

---
+++ src/Types/TR3/Textures/TR3TinnosTextureBuilder.cs
@@ -0,0 +1,37 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+using TRXInjectionTool.Util;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3TinnosTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = CreateBaseData();
+        return [data];
+    }
+
+    private static InjectionData CreateBaseData()
+    {
+        var tinnos = _control3.Read($"Resources/TR3/{TR3LevelNames.TINNOS}");
+        var thames = _control3.Read($"Resources/TR3/{TR3LevelNames.THAMES}");
+        var button = tinnos.Models[TR3Type.PushButtonSwitch];
+        ResetLevel(tinnos);
+
+        tinnos.Models[TR3Type.PushButtonSwitch] = button;
+        button.Animations = thames.Models[TR3Type.PushButtonSwitch].Animations;
+        foreach (var frame in button.Animations.SelectMany(a => a.Frames))
+        {
+            frame.Bounds = TRAnimBoundsCalculator.ComputeFrameBounds(button, frame);
+        }
+
+        button.Meshes.Clear();
+        button.MeshTrees.Clear();
+
+        var data = InjectionData.Create(tinnos, InjectionType.TextureFix, "tinnos_textures");
+        CreateDefaultTests(data, $"TR3/{TR3LevelNames.TINNOS}");
+        return data;
+    }
+}
