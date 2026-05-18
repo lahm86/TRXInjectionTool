5517d38d695798f15538325e2a0847ae86fc32e3
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-18 11:41:21 +0100

    Fix floating spikes
    This fixes the floating spikes in Madhouse.

---
+++ src/Types/TR3/Textures/TR3MadhouseTextureBuilder.cs
@@ -0,0 +1,49 @@
+﻿using TRLevelControl;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3MadhouseTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "zoo_textures");
+        CreateDefaultTests(data, $"TR3/{TR3LevelNames.MADHOUSE}");
+
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.MADHOUSE}");
+        FixSpikes(data, level);
+
+        return [data];
+    }
+
+    private static void FixSpikes(InjectionData data, TR3Level level)
+    {
+        var mesh = level.Models[TR3Type.TeethSpikesOrBarbedWire].Meshes[0];
+        var maxY = mesh.Vertices.Max(v => v.Y);
+
+        var edit = new TRMeshEdit
+        {
+            ModelID = (uint)TR3Type.TeethSpikesOrBarbedWire,
+            VertexEdits = [],
+        };
+        data.MeshEdits.Add(edit);
+
+        for (short i = 0; i < mesh.Vertices.Count; i++)
+        {
+            var vertex = mesh.Vertices[i];
+            if (vertex.Y < maxY)
+            {
+                continue;
+            }
+
+            edit.VertexEdits.Add(new()
+            {
+                Index = i,
+                Change = new() { Y = TRConsts.Step1 / 2 },
+            });
+        }
+    }
+}
