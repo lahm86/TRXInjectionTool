9fe1ab3ecbb6608c34dad2479b365104450b4dc7
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-18 14:38:35 +0100

    Fix Shakespeare Cliff textures

---
+++ src/Types/TR3/Textures/TR3CliffTextureBuilder.cs
@@ -0,0 +1,23 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3CliffTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "cliff_textures");
+
+        data.RoomEdits.AddRange(CreateRotations());
+
+        return [data];
+    }
+
+    private static IEnumerable<TRRoomTextureRotate> CreateRotations()
+    {
+        yield return Rotate(22, TRMeshFaceType.TexturedQuad, 94, 2);
+        yield return Rotate(23, TRMeshFaceType.TexturedQuad, 5, 2);
+    }
+}
