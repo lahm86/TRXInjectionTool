95edb38e517c4550e0d94036165ce34f5067deff
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-18 14:54:53 +0100

    Fix Highland Fling textures

---
+++ src/Types/TR3/Textures/TR3FlingTextureBuilder.cs
@@ -0,0 +1,54 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Textures;
+
+public class TR3FlingTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.TextureFix, "scotland_textures");
+
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.FLING}");
+        data.RoomEdits.AddRange(CreateShifts(level));
+
+        return [data];
+    }
+
+    private static IEnumerable<TRRoomTextureEdit> CreateShifts(TR3Level level)
+    {
+        var room = level.Rooms[128];
+        var vtx = room.Mesh.Vertices[room.Mesh.Rectangles[30].Vertices[2]];
+        vtx.UseCaustics = false;
+        vtx.UseWaveMovement = false;
+        yield return CreateVertex(128, room, vtx, shift: 0);
+
+        var newIdx = (ushort)(room.Mesh.Vertices.Count - 1);
+        yield return CreateQuadShift(128, 30,
+        [
+            new()
+            {
+                Index = 2,
+                NewVertexIndex = newIdx,
+            },
+        ]);
+        yield return CreateQuadShift(128, 22,
+        [
+            new()
+            {
+                Index = 3,
+                NewVertexIndex = newIdx,
+            },
+        ]);
+        yield return CreateQuadShift(128, 23,
+        [
+            new()
+            {
+                Index = 3,
+                NewVertexIndex = newIdx,
+            },
+        ]);
+    }
+}
