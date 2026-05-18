01e6c02f2a70b93e960ace0d725e8854e3eab2fd
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-18 12:13:51 +0100

    Fix missing alarm SFX

---
+++ src/Types/TR3/SFX/TR3AlarmSFXBuilder.cs
@@ -0,0 +1,17 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.SFX;
+
+public class TR3AlarmSFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var level = _control3.Read($"Resources/TR3/{TR3LevelNames.ALDWYCH}");
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, "alarm_sfx");
+        data.SFX.Add(TRSFXData.Create(TR3SFX.Alarm1, level.SoundEffects[TR3SFX.Alarm1]));
+        return [data];
+    }
+}
