90051944f6ac9c18e412de3b8de79b231bf8798d
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-05-30 18:23:47 +0100

    Add Dolphin alias
    This adds an alias for the orca as a dolphin.

---
+++ src/Types/TR3/Objects/TR3ObjectTypeBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.Objects;
+
+public class TR3ObjectTypeBuilder : InjectionBuilder
+{
+    private const TR3Type _dolphin = (TR3Type)429;
+
+    private static readonly Dictionary<string, List<TRObjectTypeEdit>> _edits = new()
+    {
+        ["undersea_objects"] =
+        [
+            new()
+            {
+                BaseType = (int)TR3Type.KillerWhale,
+                TargetType = (int)_dolphin,
+            },
+        ],
+    };
+
+    public override List<InjectionData> Build()
+    {
+        return [.. _edits.Select(kvp =>
+        {
+            var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, kvp.Key);
+            data.ObjectTypeEdits.AddRange(kvp.Value);
+            return data;
+        })];
+    }
+}
