2f0179afa6c3b2db5725f82f30bbd46ac63202d6
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-06-19 19:33:05 +0100

    Fix Aldwych portals

--- src/Types/TR3/FD/TR3AldwychFDBuilder.cs
+++ src/Types/TR3/FD/TR3AldwychFDBuilder.cs
@@ -12,6 +12,7 @@ public class TR3AldwychFDBuilder : InjectionBuilder
         var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, "aldwych_fd");
         CreateDefaultTests(data, $"TR3/{TR3LevelNames.ALDWYCH}");
         data.FloorEdits.Add(FixDrillAntitrigger());
+        data.FloorEdits.AddRange(FixPoolPortals());
         data.ItemPosEdits.Add(FixPunkFire());
 
         return [data];
@@ -37,5 +38,19 @@ public class TR3AldwychFDBuilder : InjectionBuilder
         fire.Y = punk.Y;
         fire.Z = punk.Z;
         return ItemBuilder.MoveToRoom(level, 200, punk.Room);
+    }
+
+    private static IEnumerable<TRFloorDataEdit> FixPoolPortals()
+    {
+        return new[] { 1, 3 }.Select(z => new TRFloorDataEdit
+        {
+            RoomIndex = 87,
+            X = 3,
+            Z = (ushort)z,
+            Fixes = [new FDPortalOverwrite
+            {
+                Sky = 60,
+            }],
+        });        
     }
 }
