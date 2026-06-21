94079e53588bebf3492c7cb7c3afdedf67edcc1b
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-06-21 13:58:26 +0100

    Add TR1 PS1 shimmy SFX
    This adds a layered shimmy SFX option for TR1 to include the PS1 version.

---
+++ src/Types/TR1/SFX/TR1ShimmySFXBuilder.cs
@@ -0,0 +1,20 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR1.SFX;
+
+public class TR1ShimmySFXBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var caves = _control1.Read($"Resources/{TR1LevelNames.CAVES}");
+        var fx = caves.SoundEffects[TR1SFX.LaraShimmy2];
+        fx.Samples = [.. Directory.GetFiles("Resources/TR1/Lara/Shimmy").Select(File.ReadAllBytes)];
+        ResetLevel(caves);
+        caves.SoundEffects[TR1SFX.LaraShimmy2] = fx;
+
+        var data = InjectionData.Create(caves, InjectionType.PS1SFX, "shimmy_sfx");
+        return [data];
+    }
+}
