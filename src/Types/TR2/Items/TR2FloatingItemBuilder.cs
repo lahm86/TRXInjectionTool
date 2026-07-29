35783d8234d6fdd7fa0d5959d0b03db4c8ed1fd3
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-07-29 20:32:57 +0100

    Swap Floating Islands secrets

--- src/Types/TR2/Items/TR2FloatingItemBuilder.cs
+++ src/Types/TR2/Items/TR2FloatingItemBuilder.cs
@@ -1,5 +1,6 @@
 ﻿using TRLevelControl.Helpers;
 using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
 using TRXInjectionTool.Control;
 
 namespace TRXInjectionTool.Types.TR2.Items;
@@ -16,8 +17,43 @@ public class TR2FloatingItemBuilder : ItemBuilder
         [
             SetAngle(floating, 1, -32768),
             MoveToRoom(floating, 95, 117), // Move the switch to the correct room
+            .. SwapSecrets(floating),
         ];
 
         return [data];
+    }
+
+    private static IEnumerable<TRItemPosEdit> SwapSecrets(TR2Level level)
+    {
+        var stoneIdx = (short)level.Entities.FindIndex(e => e.TypeID == TR2Type.StoneSecret_S_P);
+        var jadeIdx = (short)level.Entities.FindIndex(e => e.TypeID == TR2Type.JadeSecret_S_P);
+
+        var jade = level.Entities[jadeIdx];
+        yield return new()
+        {
+            Index = stoneIdx,
+            Item = new()
+            {
+                Angle = jade.Angle,
+                X = jade.X,
+                Y = jade.Y,
+                Z = jade.Z,
+                Room = jade.Room,
+            },
+        };
+
+        var stone = level.Entities[stoneIdx];
+        yield return new()
+        {
+            Index = jadeIdx,
+            Item = new()
+            {
+                Angle = stone.Angle,
+                X = stone.X,
+                Y = stone.Y,
+                Z = stone.Z,
+                Room = stone.Room,
+            },
+        };
     }
 }
