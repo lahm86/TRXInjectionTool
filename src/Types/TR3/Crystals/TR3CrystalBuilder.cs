78fb1363bab0964c86da73c221352e6836ef841d
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-08-15 14:20:04 +0100

    Add TR3LA crystals
    This adds crystal pickup locations to each of the TR3LA levels.

--- src/Types/TR3/Crystals/TR3CrystalBuilder.cs
+++ src/Types/TR3/Crystals/TR3CrystalBuilder.cs
@@ -1,3 +1,5 @@
+using TRLevelControl;
+using TRLevelControl.Model;
 using TRXInjectionTool.Control;
 using TRXInjectionTool.Util;
 
@@ -6,5 +8,21 @@ namespace TRXInjectionTool.Types.TR3.Crystals;
 public class TR3CrystalBuilder : InjectionBuilder
 {
     public override List<InjectionData> Build()
-        => [CrystalUtils.MakeTR3Crystal()];
+        => [.. CreatePlacements(), CrystalUtils.MakeTR3Crystal()];
+
+    private static IEnumerable<InjectionData> CreatePlacements()
+    {
+        var crystals = CrystalUtils.GetLocations("Resources/TR3/crystals.json", TR3Type.SaveCrystal_P);
+        return crystals.Where(kvp => kvp.Value.Count > 0).Select(kvp =>
+        {
+            var level = _control3.Read($"Resources/TR3/{kvp.Key}");
+            var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.General, $"{_tr3NameMap[kvp.Key]}_crystals");
+            CreateDefaultTests(data, $"TR3/{kvp.Key}");
+
+            // TR3 requires crystals to be triggered or otherwise active by default.
+            kvp.Value.ForEach(i => i.Flags = TRConsts.FullMask << 9);
+            data.FloorEdits.AddRange(CrystalUtils.ConvertItems(kvp.Value, r => level.Rooms[r].Info));
+            return data;
+        });
+    }
 }
