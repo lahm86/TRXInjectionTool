ad21a2a08b3ab0047ec50599e280910d0a5a53f5
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-08-24 18:29:13 +0100

    Fix Antarctica portal

---
+++ src/Types/TR3/FD/TR3AntarcticaFDBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.FD;
+
+public class TR3AntarcticaFDBuilder : InjectionBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.FDFix, "antarc_fd");
+        CreateDefaultTests(data, $"TR3/{TR3LevelNames.ANTARC}");
+        data.FloorEdits.Add(FixRoom20Portal());
+
+        return [data];
+    }
+
+    private static TRFloorDataEdit FixRoom20Portal()
+    {
+        return new TRFloorDataEdit
+        {
+            RoomIndex = 20,
+            X = 6,
+            Z = 6,
+            Fixes = [new FDPortalOverwrite
+            {
+                Sky = 19,
+            }],
+        };
+    }
+}
