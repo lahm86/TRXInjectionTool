37947dd046b44cfd32edc6fa194f1f74a6e453a5
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-09-02 18:09:58 +0100

    Fix Compound ladder

---
+++ src/Types/TR3/FD/TR3CompoundFDBuilder.cs
@@ -0,0 +1,32 @@
+﻿using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR3.FD;
+
+public class TR3CompoundFDBuilder : FDBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR3, InjectionType.FDFix, "compound_fd");
+        CreateDefaultTests(data, $"TR3/{TR3LevelNames.HSC}");
+        data.FloorEdits.Add(FixLadder93());
+
+        return [data];
+    }
+
+    private static TRFloorDataEdit FixLadder93()
+    {
+        return new()
+        {
+            RoomIndex = 93,
+            X = 5,
+            Z = 2,
+            Fixes = [new FDPortalOverwrite
+            {
+                Wall = 87,
+            }],
+        };
+    }
+}
