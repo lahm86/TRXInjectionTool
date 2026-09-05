8d0e88b7973faa23ea727420a2fb5bf290f602c9
lahm86 <33758420+lahm86@users.noreply.github.com>
2026-09-05 16:18:53 +0100

    Fix title/Cambodia texture issues

---
+++ src/Types/TR4/FD/TR4AngkorFDBuilder.cs
@@ -0,0 +1,57 @@
+﻿using TRLevelControl;
+using TRLevelControl.Helpers;
+using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR4.FD;
+
+public class TR4AngkorFDBuilder : FDBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR4, InjectionType.FDFix, "angkor_fd");
+        CreateDefaultTests(data, $"TR4/{TR4LevelNames.ANGKOR}");
+        FixRoom60Portal(data);
+
+        return [data];
+    }
+
+    private static void FixRoom60Portal(InjectionData data)
+    {
+        data.VisPortalEdits.Add(new()
+        {
+            BaseRoom = 60,
+            LinkRoom = 64,
+            PortalIndex = 2,
+            VertexChanges =
+            [
+                new() { Z = -TRConsts.Step4 },
+                new(),
+                new(),
+                new() { Z = -TRConsts.Step4 },
+            ],
+        });
+        data.VisPortalEdits.Add(new()
+        {
+            BaseRoom = 64,
+            LinkRoom = 60,
+            PortalIndex = 2,
+            VertexChanges =
+            [
+                new() { Z = -TRConsts.Step4 },
+                new() { Z = -TRConsts.Step4 },
+                new(),
+                new(),
+            ],
+        });
+
+        data.FloorEdits.Add(new()
+        {
+            RoomIndex = 64,
+            X = 1,
+            Z = 2,
+            Fixes = [new FDPortalOverwrite { Sky = -1, Pit = 69 }],
+        });
+    }
+}
---
+++ src/Types/TR4/Textures/TR4TitleTextureBuilder.cs
@@ -0,0 +1,24 @@
+﻿using TRLevelControl.Model;
+using TRXInjectionTool.Actions;
+using TRXInjectionTool.Control;
+
+namespace TRXInjectionTool.Types.TR4.Textures;
+
+public class TR4TitleTextureBuilder : TextureBuilder
+{
+    public override List<InjectionData> Build()
+    {
+        var data = InjectionData.Create(TRGameVersion.TR4, InjectionType.TextureFix, "title_textures");
+        CreateDefaultTests(data, "TR4/title.tr4");
+        data.RoomEdits.Add(FixRoom30());
+
+        return [data];
+    }
+
+    private static TRRoomTextureReface FixRoom30()
+    {
+        var level = _control4.Read($"Resources/TR4/title.tr4");
+        var tex = level.Rooms[30].Mesh.Rectangles[9].Texture;
+        return Reface(level, 30, TRMeshFaceType.TexturedQuad, TRMeshFaceType.TexturedQuad, tex, 14);
+    }
+}
